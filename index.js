29a1ea9aea87846e3f14ffb9cd4b4c5a5f1903eb
Paul Alarcon <pa.f.e@hotmail.com>
2022-06-19 20:28:02 -0400

    visualizing json objects

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// console.log("hello");
-
 const obj1Div = document.getElementsByClassName("obj1")[0];
-
 let root = document.getElementsByClassName("root")[0];
-let txt = document.createTextNode("Art Institute of Chicago API");
-
-root.appendChild(document.createElement("h1").appendChild(txt));
-
+// let rootClasses = "relative overflow-x-auto shadow-md";
+// root.classList.add(...rootClasses.split(" "));
 const API_URL = "https://api.artic.edu/api/v1/artworks";
 
 const getData = async (url) => {
   const response = await fetch(url);
   if (response.status == 200 && response.ok) {
     const responseJson = await response.json();
-    // console.log(responseJson);
     return responseJson;
   }
   return {
@@ -21,30 +15,74 @@ const getData = async (url) => {
   };
 };
 
+const createRowWithDataFromObj = (objK, objV) => {
+  const row = document.createElement("tr");
+  // let rowClasses =
+  //   "bg-white border-b dark:bg-gray-800 dark:border-gray-700".split(" ");
+  // row.classList.add(...rowClasses);
+  const keyCell = document.createElement("td");
+  row.appendChild(keyCell);
+  const valueCell = document.createElement("td");
+  row.appendChild(valueCell);
+
+  keyCell.appendChild(document.createTextNode(objK));
+
+  if (typeof objV == "object" && objV != null) {
+    const objVKeys = Object.keys(objV);
+    objVKeys.forEach((k) => {
+      valueCell.appendChild(createRowWithDataFromObj(k, objV[k]));
+    });
+    return row;
+  } else {
+    valueCell.appendChild(document.createTextNode(objV));
+    return row;
+  }
+};
+
+const createHeaderRow = (headersArr) => {
+  const headerRow = document.createElement("tr");
+  headersArr.forEach((header) => {
+    const th = document.createElement("th");
+    th.appendChild(document.createTextNode(header));
+    headerRow.appendChild(th);
+  });
+  return headerRow;
+};
+
 const dinamycallyDisplayAnObjectOntoHTML = (obj) => {
+  if (obj == null) return;
+  const table = document.createElement("table");
+  // let tableClasses = "text-sm text-left text-gray-500 dark:text-gray-400".split(
+  //   " "
+  // );
+  // table.classList.add(...tableClasses);
+  const headerRow = createHeaderRow(["key", "value"]);
+  // let headerRowClasses =
+  //   "bg-white border-b dark:bg-gray-800 dark:border-gray-700".split(" ");
+  // headerRow.classList.add(...headerRowClasses);
+  table.appendChild(headerRow);
+
   const objKeys = Object.keys(obj);
   objKeys.forEach((objK) => {
-    let objDiv = document.createElement("div");
-    let keyDiv = document.createElement("span");
-    keyDiv.appendChild(document.createTextNode(objK));
-    let valueDiv = document.createElement("span");
-    let objValue = obj[objK];
-    valueDiv.appendChild(document.createTextNode(objValue));
-    objDiv.appendChild(keyDiv);
-    objDiv.appendChild(valueDiv);
-
-    obj1Div.appendChild(objDiv);
+    let currRow = createRowWithDataFromObj(objK, obj[objK]);
+    table.appendChild(currRow);
   });
+  obj1Div.appendChild(table);
 };
 
 const main = async () => {
   const data = await getData(API_URL);
-  //   console.log(data);
-  //   console.log(Object.keys(data));
   let arrData = data.data;
-  console.log(arrData[0]);
-  dinamycallyDisplayAnObjectOntoHTML(arrData[0]);
-  //   arrData.forEach((x) => console.log(x));
+  dinamycallyDisplayAnObjectOntoHTML(arrData[1]);
+  // const data = {
+  //   name: "paul",
+  //   age: 21,
+  //   education: {
+  //     college: "Queens College",
+  //     secondary: "Juan Montalvo",
+  //   },
+  // };
+  // dinamycallyDisplayAnObjectOntoHTML(data);
 };
 
 main();
